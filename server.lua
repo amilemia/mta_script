34d4f6b6126074ad062e695c363bf3fbc963ebb8
Mohammed-Amine <125489574+amilemia@users.noreply.github.com>
2025-06-19 03:27:35 +0100

    Implement server-side loading of attachments

--- server.lua
+++ server.lua
@@ -17,6 +17,38 @@ addEventHandler("attachObjectToVehicle", resourceRoot, function(veh, modelID, ox
     triggerClientEvent(client, "onObjectAttached", resourceRoot, obj)
 end)
 
+-- Create multiple attachments from client provided data
+addEvent("loadVehicleAttachments", true)
+addEventHandler("loadVehicleAttachments", resourceRoot, function(veh, attachments)
+    if not isElement(veh) or type(attachments) ~= "table" then
+        return
+    end
+
+    for _, data in ipairs(attachments) do
+        local modelID = tonumber(data.id)
+        if modelID and engineGetModelNameFromID(modelID) then
+            local x = tonumber(data.x) or 0
+            local y = tonumber(data.y) or 0
+            local z = tonumber(data.z) or 0
+            local rx = tonumber(data.rx) or 0
+            local ry = tonumber(data.ry) or 0
+            local rz = tonumber(data.rz) or 0
+            local sx = tonumber(data.sx) or 1
+            local sy = tonumber(data.sy) or sx
+            local sz = tonumber(data.sz) or sx
+
+            local obj = createObject(modelID, 0, 0, 0)
+            attachElements(obj, veh, x, y, z, rx, ry, rz)
+            setObjectScale(obj, sx, sy, sz)
+
+            vehicleAttachments[veh] = vehicleAttachments[veh] or {}
+            table.insert(vehicleAttachments[veh], obj)
+
+            triggerClientEvent(client, "onObjectAttached", resourceRoot, obj)
+        end
+    end
+end)
+
 local function cleanupVehicleAttachments(veh)
     if vehicleAttachments[veh] then
         for _, obj in ipairs(vehicleAttachments[veh]) do
